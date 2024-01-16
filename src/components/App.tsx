3762ee30497e64cbd19201f1f8cb7ea98bffef81
Yehor Korablin <yegor_ka2004@ukr.net>
2024-01-16 18:42:45 +0200

    Fixed code style

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import logo from '../logo.svg';
 import './App.css';
 
-
-
-const App: React.FC = () => {
+export const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -13,5 +11,3 @@ const App: React.FC = () => {
     </div>
   );
 }
-
-export default App;
