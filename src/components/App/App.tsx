9d02afbbfcdee49c283dda4a96c80b5e92ec5c13
Yehor Korablin <yegor_ka2004@ukr.net>
2024-01-16 20:26:30 +0200

    Fixed code style

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import logo from '../../logo.svg';
 import './App.css';
 
-export const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+export const App: React.FC = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
 
-        Hello React!
-      </header>
-    </div>
-  );
-}
+      Hello React!
+    </header>
+  </div>
+);
